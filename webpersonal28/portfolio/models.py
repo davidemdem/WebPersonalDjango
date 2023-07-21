31012edec70f888caa04a7c60bc24ad00b319f9d
David Medina <demacdc6@gmail.com>
2023-07-21 00:20:57 -0300

    añadiendo el proyecto v1

---
+++ webpersonal28/portfolio/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200, verbose_name = "Tituto")
+    description = models.TextField(verbose_name="Descripcion")
+    image = models.ImageField(verbose_name="Imagen", upload_to="projects")
+    link = models.URLField(verbose_name="pagina web",null=True,blank=True)
+    created = models.DateTimeField(auto_now_add=True,verbose_name="Fecha de Creacion")
+    updated = models.DateTimeField(auto_now=True,verbose_name="Fecha de Modificacion")
+    
+    def __str__(self):
+        return self.title  
+    
+    
+    class Meta:
+        verbose_name = "proyecto"
+        verbose_name_plural = "proyectos"
+        ordering =['-created']
+        
+    
+    
